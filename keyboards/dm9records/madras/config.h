f34ec90674f4b539a7016c8dd8d5e043020d7f03
hsgw <urkwtky@gmail.com>
2025-03-16 03:50:25 +0900

    add dm9records/madras

---
+++ keyboards/dm9records/madras/config.h
@@ -0,0 +1,12 @@
+// Copyright 2025 Takuya Urakawa @hsgw (dm9records.com, 5z6p.com)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/* led */
+#define LED1 D0
+#define LED2 D1
+
+/* haptic */
+#define SOLENOID_PIN C5
+#define SOLENOID_DEFAULT_DWELL 10
---
+++ keyboards/dm9records/madras/keymaps/vial/config.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID \
+    { 0xD8, 0x6D, 0xDA, 0x0E, 0x88, 0x1B, 0x26, 0x9D }
+
+#define VIAL_UNLOCK_COMBO_ROWS \
+    { 0, 3 }
+
+#define VIAL_UNLOCK_COMBO_COLS \
+    { 0, 9 }
+
+#define LED_NUM_LOCK_PIN LED1
+#define HAPTIC_ENABLE_STATUS_LED LED2
